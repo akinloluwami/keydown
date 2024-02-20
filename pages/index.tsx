984314fe4b49d68ff0492334f1c95ccaee87ca24
akinloluwami <akinkunmioye42@gmail.com>
2024-02-20 23:56:22 +0100

    move div to component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
+import Header from "@/components/Header";
 import Link from "next/link";
 import React from "react";
 
 const Home = () => {
   return (
     <div className="h-screen flex flex-col justify-between">
-      <div className="border-b border-dashed border-that-grey px-10 py-5">
-        <h3 className="font-bold text-lg">keydown</h3>
-      </div>
+      <Header />
       <div className="flex flex-col items-center gap-y-10">
         <h1 className="text-center font-bold text-7xl max-w-[500px] mx-auto">
           What will you write today?
