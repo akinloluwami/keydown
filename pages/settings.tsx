75e7095c73e18655b3e3e1a8b4b71c3bce727c07
akinloluwami <akinkunmioye42@gmail.com>
2024-02-21 13:28:02 +0100

    settings

---
+++ pages/settings.tsx
@@ -0,0 +1,8 @@
+import DashboardLayout from "@/layouts/Dashboard";
+import React from "react";
+
+const Settings = () => {
+  return <DashboardLayout>Settings</DashboardLayout>;
+};
+
+export default Settings;
