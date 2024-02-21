90ebdecd6a12795ebf846bb17db13c6c1a803ba6
akinloluwami <akinkunmioye42@gmail.com>
2024-02-21 15:00:40 +0100

    me settings

---
+++ pages/settings/me.tsx
@@ -0,0 +1,13 @@
+import SettingsLinks from "@/components/SettingsLinks";
+import DashboardLayout from "@/layouts/Dashboard";
+import React from "react";
+
+const MeSettings = () => {
+  return (
+    <DashboardLayout>
+      <SettingsLinks />
+    </DashboardLayout>
+  );
+};
+
+export default MeSettings;
