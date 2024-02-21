3187d946d40dfcde4d2d4ddf65e54f9a364eede4
akinloluwami <akinkunmioye42@gmail.com>
2024-02-21 13:27:34 +0100

    dashboard

---
+++ pages/dashboard.tsx
@@ -0,0 +1,8 @@
+import DashboardLayout from "@/layouts/Dashboard";
+import React from "react";
+
+const Dashboard = () => {
+  return <DashboardLayout>Dashboard</DashboardLayout>;
+};
+
+export default Dashboard;
