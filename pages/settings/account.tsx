f1d14e38c28a905df14f7469ff80e40cc0a65a87
akinloluwami <akinkunmioye42@gmail.com>
2024-02-21 15:00:21 +0100

    account settings

---
+++ pages/settings/account.tsx
@@ -0,0 +1,13 @@
+import SettingsLinks from "@/components/SettingsLinks";
+import DashboardLayout from "@/layouts/Dashboard";
+import React from "react";
+
+const AccountSettings = () => {
+  return (
+    <DashboardLayout>
+      <SettingsLinks />
+    </DashboardLayout>
+  );
+};
+
+export default AccountSettings;
