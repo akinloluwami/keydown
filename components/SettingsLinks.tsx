9d034a06455d767a66c5b2f481bb0e11bf0b4838
akinloluwami <akinkunmioye42@gmail.com>
2024-02-21 15:00:00 +0100

    links

---
+++ components/SettingsLinks.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React from "react";
+
+const SettingsLinks = () => {
+  const pages = [
+    { name: "Blog", route: "/settings" },
+    { name: "Me", route: "/settings/me" },
+    { name: "Account", route: "/settings/account" },
+    { name: "Password", route: "/settings/password" },
+  ];
+  const router = useRouter();
+  return (
+    <div className="flex items-center justify-center gap-x-36 w-full text-lg">
+      {pages.map((page, i) => (
+        <Link href={page.route} key={i}>
+          {router.pathname === page.route && <span className="mr-2">&gt;</span>}
+          {page.name}
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default SettingsLinks;
