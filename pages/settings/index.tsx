01b9213b03a152c0e48790ceb909584442e6a31a
akinloluwami <akinkunmioye42@gmail.com>
2024-02-21 15:00:30 +0100

    blog settings

---
+++ pages/settings/index.tsx
@@ -0,0 +1,13 @@
+import SettingsLinks from "@/components/SettingsLinks";
+import DashboardLayout from "@/layouts/Dashboard";
+import React from "react";
+
+const Settings = () => {
+  return (
+    <DashboardLayout>
+      <SettingsLinks />
+    </DashboardLayout>
+  );
+};
+
+export default Settings;
