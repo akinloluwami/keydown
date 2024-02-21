90304841f665c5979de2bf16135b16692c2dc0df
akinloluwami <akinkunmioye42@gmail.com>
2024-02-21 13:28:23 +0100

    write

---
+++ pages/write.tsx
@@ -0,0 +1,8 @@
+import DashboardLayout from "@/layouts/Dashboard";
+import React from "react";
+
+const Write = () => {
+  return <DashboardLayout>Write</DashboardLayout>;
+};
+
+export default Write;
