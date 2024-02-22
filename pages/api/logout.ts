53f6052a038d29a61641823a34e65a91b8da5659
akinloluwami <akinkunmioye42@gmail.com>
2024-02-23 00:06:41 +0100

    logout

---
+++ pages/api/logout.ts
@@ -0,0 +1,22 @@
+import { lucia } from "@/lib/lucia";
+import { validateRequest } from "@/utils/validateRequest";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== "POST") {
+    res.status(404).end();
+    return;
+  }
+  const { session } = await validateRequest(req, res);
+  if (!session) {
+    res.status(401).end();
+    return;
+  }
+  await lucia.invalidateSession(session.id);
+  res
+    .setHeader("Set-Cookie", lucia.createBlankSessionCookie().serialize())
+    .status(200)
+    .end();
+};
+
+export default handler;
