444d04b6e435f3692f9dcb731def9abe8b08254c
akinloluwami <akinkunmioye42@gmail.com>
2024-02-22 18:22:51 +0100

    /api/signup

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,34 +1,64 @@
 import Header from "@/components/Header";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
+import { toast } from "sonner";
 
 const Signup = () => {
+  const router = useRouter();
   return (
     <div className="h-screen flex flex-col justify-between">
       <Header />
       <div className="flex flex-col items-center justify-center gap-y-2">
         <h1 className="text-6xl font-bold ">Signup</h1>
-        <form className="flex flex-col gap-y-4 mt-10 w-full max-w-[500px]">
+        <form
+          className="flex flex-col gap-y-4 mt-10 w-full max-w-[500px]"
+          action="/api/signup"
+          method="POST"
+          onSubmit={async (e) => {
+            e.preventDefault();
+            const formElement = e.target as HTMLFormElement;
+            const response = await fetch(formElement.action, {
+              method: formElement.method,
+              body: JSON.stringify(
+                Object.fromEntries(new FormData(formElement).entries())
+              ),
+              headers: {
+                "Content-Type": "application/json",
+              },
+            });
+            if (response.ok) {
+              router.push("/dashboard");
+            } else {
+              const data = await response.json();
+              toast.error(data.message);
+            }
+          }}
+        >
           <input
             type="text"
             className="bg-transparent border border-dashed border-that-grey text-xl pl-3 py-3 placeholder:text-that-grey font-semibold"
             placeholder="Email"
+            name="email"
           />
           <input
             type="text"
             className="bg-transparent border border-dashed border-that-grey text-xl pl-3 py-3 placeholder:text-that-grey font-semibold"
             placeholder="First name"
+            name="firstname"
           />
           <input
             type="password"
             className="bg-transparent border border-dashed border-that-grey text-xl pl-3 py-3 placeholder:text-that-grey font-semibold"
             placeholder="Password"
+            name="password"
           />
           <div className="flex items-center border border-dashed border-that-grey text-xl pl-3 py-3">
             <input
               type="text"
               className="bg-transparent outline-none text-right  placeholder:text-that-grey font-semibold"
               placeholder="username"
+              name="username"
             />
             <p className="text-xl font-semibold">.keydown.co</p>
           </div>
