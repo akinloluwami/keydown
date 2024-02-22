efaf653ea195642f06ca7a964f9d20624d25914d
akinloluwami <akinkunmioye42@gmail.com>
2024-02-22 12:49:03 +0100

    signup

---
+++ pages/api/signup.ts
@@ -0,0 +1,105 @@
+import { db, users } from "@/lib/db";
+import { lucia } from "@/lib/lucia";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Argon2id } from "oslo/password";
+import validator from "validator";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== "POST") {
+    res.status(404).end();
+    return;
+  }
+  const body: null | Partial<{
+    username: string;
+    password: string;
+    email: string;
+    firstname: string;
+  }> = req.body;
+
+  const username = body?.username;
+  const password = body?.password;
+  const email = body?.email;
+  const firstname = body?.firstname;
+
+  if (!username) {
+    res.status(400).json({ message: "Username is required" });
+    return;
+  }
+
+  if (!password) {
+    res.status(400).json({ message: "Password is required" });
+    return;
+  }
+
+  if (!email) {
+    res.status(400).json({ message: "Email is required" });
+    return;
+  }
+
+  if (!firstname) {
+    res.status(400).json({ message: "Firstname is required" });
+    return;
+  }
+
+  if (!validator.isEmail(email)) {
+    res.status(400).json({ message: "Email is invalid" });
+    return;
+  }
+
+  if (password.length < 8) {
+    res.status(400).json({ message: "Password must be at least 8 characters" });
+    return;
+  }
+
+  if (password.length > 256) {
+    res
+      .status(400)
+      .json({ message: "Password must be less than 256 characters" });
+    return;
+  }
+
+  if (!validator.isAlphanumeric(username)) {
+    res.status(400).json({ message: "Username must be alphanumeric" });
+    return;
+  }
+
+  if (username.length < 3) {
+    res.status(400).json({ message: "Username must be at least 3 characters" });
+    return;
+  }
+
+  if (username.length > 25) {
+    res
+      .status(400)
+      .json({ message: "Username must be less than 25 characters" });
+    return;
+  }
+
+  const hashedPassword = await new Argon2id().hash(password);
+
+  try {
+    type NewUser = typeof users.$inferInsert;
+    const insertUser = async (user: NewUser) => {
+      return db.insert(users).values(user);
+    };
+    const newUser: NewUser = {
+      email,
+      firstname,
+      username,
+      password: hashedPassword,
+    };
+    await insertUser(newUser);
+
+    const session = await lucia.createSession(newUser.id!, {});
+    res
+      .appendHeader(
+        "Set-Cookie",
+        lucia.createSessionCookie(session.id).serialize()
+      )
+      .status(200)
+      .end();
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+export default handler;
