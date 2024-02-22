0927db51f8d9d2a0a75fd200ea4975e9b256bf25
akinloluwami <akinkunmioye42@gmail.com>
2024-02-22 12:47:27 +0100

    create tables

---
+++ lib/db.ts
@@ -0,0 +1,35 @@
+import { DrizzlePostgreSQLAdapter } from "@lucia-auth/adapter-drizzle";
+import pg from "pg";
+import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { drizzle } from "drizzle-orm/node-postgres";
+
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export const users = pgTable("users", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  email: text("email").unique().notNull(),
+  username: text("username").unique().notNull(),
+  password: text("password").notNull(),
+  firstname: text("firstname").notNull(),
+});
+
+export const sessions = pgTable("sessions", {
+  id: uuid("id").primaryKey(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => users.id),
+  expiresAt: timestamp("expires_at", {
+    withTimezone: true,
+    mode: "date",
+  }).notNull(),
+});
+
+export const db = drizzle(pool, {
+  schema: {
+    users,
+  },
+});
+
+export const adapter = new DrizzlePostgreSQLAdapter(db, sessions, users);
