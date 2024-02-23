2344a1b30207091fb844528533341c24d026904f
akinloluwami <akinkunmioye42@gmail.com>
2024-02-23 02:07:27 +0100

    validate data

--- pages/api/settings/index.ts
+++ pages/api/settings/index.ts
@@ -2,6 +2,7 @@ import { db, users } from "@/lib/db";
 import { validateRequest } from "@/utils/validateRequest";
 import { eq } from "drizzle-orm";
 import { NextApiRequest, NextApiResponse } from "next";
+import validator from "validator";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { user } = await validateRequest(req, res);
@@ -25,6 +26,27 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "PUT") {
     const { subdomain, title, description } = req.body;
 
+    if (!subdomain || !title) {
+      res.status(400).json({
+        message: "Username and blog title are required",
+      });
+      return;
+    }
+
+    if (subdomain.length < 3) {
+      res.status(400).json({
+        message: "Username must be at least 3 characters long",
+      });
+      return;
+    }
+
+    if (validator.isNumeric(subdomain)) {
+      res.status(400).json({
+        message:
+          "Username must start be alphanumeric when starting with a number",
+      });
+      return;
+    }
     await db
       .update(users)
       .set({
@@ -35,7 +57,9 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       })
       .where(eq(users.id, user.id));
 
-    return res.status(200).end();
+    return res.status(200).json({
+      message: "Blog settings updated successfully",
+    });
   }
 };
 
