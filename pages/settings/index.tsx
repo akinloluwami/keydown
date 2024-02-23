b6e97aad3e6b154c13cd96f3698e56fbd7be88e7
akinloluwami <akinkunmioye42@gmail.com>
2024-02-23 02:07:47 +0100

    ___

--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -1,12 +1,36 @@
 import SettingsLinks from "@/components/SettingsLinks";
 import DashboardLayout from "@/layouts/Dashboard";
-import React from "react";
+import { fetcher } from "@/lib/fetcher";
+import axios from "axios";
+import React, { useState } from "react";
+import { toast } from "sonner";
+import useSWR from "swr";
 
 const Settings = () => {
+  const { data, isLoading } = useSWR("/api/settings", fetcher);
+
+  const [loading, setLoading] = useState(false);
   return (
     <DashboardLayout>
       <SettingsLinks />
-      <div className="w-full font-semibold max-w-[500px] mt-20 flex flex-col gap-y-10">
+      <form
+        className="w-full font-semibold max-w-[500px] mt-20 flex flex-col gap-y-10"
+        onSubmit={async (e) => {
+          setLoading(true);
+          e.preventDefault();
+          try {
+            await axios.put(
+              "/api/settings",
+              Object.fromEntries(
+                new FormData(e.target as HTMLFormElement).entries()
+              )
+            );
+            toast.success("Blog setting updated");
+          } catch (error: any) {
+            toast.error(error.response.data.message || "Something went wrong");
+          }
+        }}
+      >
         <div className="">
           <p className="text-lg mb-2">Blog subdomain</p>
           <div className="flex items-center border border-dashed border-that-grey text-xl pl-3 py-3">
@@ -14,6 +38,8 @@ const Settings = () => {
               type="text"
               className="bg-transparent outline-none text-right  placeholder:text-that-grey font-semibold"
               placeholder="username"
+              defaultValue={data?.subdomain}
+              name="subdomain"
             />
             <p className="text-xl font-semibold text-that-grey-1">
               .keydown.co
@@ -30,6 +56,8 @@ const Settings = () => {
             type="text"
             className="bg-transparent w-full border border-dashed border-that-grey text-xl pl-3 py-3 placeholder:text-that-grey font-semibold"
             placeholder="blog title"
+            defaultValue={data?.title}
+            name="title"
           />
           <p className="font-normal mt-1 text-that-grey-1">
             The blog title will replace your name on blog pages.
@@ -40,13 +68,15 @@ const Settings = () => {
           <textarea
             className="w-full bg-transparent border border-dashed border-that-grey text-xl pl-3 py-3 placeholder:text-that-grey font-semibold"
             placeholder="What is your blog about?"
+            defaultValue={data?.description}
+            name="description"
           />
           <p className="font-normal mt-1 text-that-grey-1">About your blog.</p>
         </div>
         <button className="bg-white text-black py-3 font-semibold text-xl w-fit px-10">
           Save changes
         </button>
-      </div>
+      </form>
     </DashboardLayout>
   );
 };
