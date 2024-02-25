a67a9a4e253ea86f1bebd74f55a4620450bad9fa
akinloluwami <akinkunmioye42@gmail.com>
2024-02-25 20:48:52 +0100

    request to endpoint

--- pages/settings/me.tsx
+++ pages/settings/me.tsx
@@ -1,9 +1,11 @@
 import SettingsLinks from "@/components/SettingsLinks";
 import DashboardLayout from "@/layouts/Dashboard";
 import { fetcher } from "@/lib/fetcher";
+import axios from "axios";
 import React, { useState } from "react";
 import { CgSpinner } from "react-icons/cg";
 import { SiGithub, SiInstagram, SiThreads, SiTwitter } from "react-icons/si";
+import { toast } from "sonner";
 import useSWR from "swr";
 
 const MeSettings = () => {
@@ -15,7 +17,26 @@ const MeSettings = () => {
   return (
     <DashboardLayout>
       <SettingsLinks />
-      <div className="w-full font-semibold max-w-[500px] mt-20 flex flex-col gap-y-10">
+      <form
+        className="w-full font-semibold max-w-[500px] mt-20 flex flex-col gap-y-10"
+        onSubmit={async (e) => {
+          setLoading(true);
+          e.preventDefault();
+          try {
+            await axios.put(
+              "/api/settings/me",
+              Object.fromEntries(
+                new FormData(e.target as HTMLFormElement).entries()
+              )
+            );
+            toast.success("Profile setting updated");
+          } catch (error: any) {
+            toast.error(error.response.data.message || "Something went wrong");
+          } finally {
+            setLoading(false);
+          }
+        }}
+      >
         <div className="">
           <p className="text-lg mb-2">First name</p>
           <input
@@ -79,7 +100,7 @@ const MeSettings = () => {
           <input
             type="text"
             className="bg-transparent w-full border border-dashed border-that-grey text-xl pl-3 py-3 placeholder:text-that-grey font-semibold"
-            placeholder={exampleUsername}
+            placeholder={`e.g github.com/${user?.firstname.toLowerCase()}`}
           />
         </div>
 
@@ -90,7 +111,7 @@ const MeSettings = () => {
             "Save changes"
           )}
         </button>
-      </div>
+      </form>
     </DashboardLayout>
   );
 };
