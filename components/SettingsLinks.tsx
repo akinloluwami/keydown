b7a151e3c7206c44caaba6663115162229f3a5b2
akinloluwami <akinkunmioye42@gmail.com>
2024-02-27 07:20:55 +0100

    update indicator

--- components/SettingsLinks.tsx
+++ components/SettingsLinks.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
+import { FaCaretRight } from "react-icons/fa";
 
 const SettingsLinks = () => {
   const pages = [
@@ -14,8 +15,12 @@ const SettingsLinks = () => {
   return (
     <div className="flex items-center justify-between font-semibold gap-x-32 w-full text-lg">
       {pages.map((page, i) => (
-        <Link href={page.route} key={i}>
-          {router.pathname === page.route && <span className="mr-2">&gt;</span>}
+        <Link href={page.route} key={i} className="flex items-center">
+          {router.pathname === page.route && (
+            <span className="mr-2">
+              <FaCaretRight />
+            </span>
+          )}
           {page.name}
         </Link>
       ))}
