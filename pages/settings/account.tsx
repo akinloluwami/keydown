34c93c1005b72caefee7707aa4dd30ad3c46a3ef
akinloluwami <akinkunmioye42@gmail.com>
2024-02-27 07:16:37 +0100

    add page title

--- pages/settings/account.tsx
+++ pages/settings/account.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const AccountSettings = () => {
   return (
-    <DashboardLayout>
+    <DashboardLayout loading={false} title="Account Settings">
       <SettingsLinks />
     </DashboardLayout>
   );
