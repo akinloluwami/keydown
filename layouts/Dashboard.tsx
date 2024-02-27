f515d8609463497bc553414d7437829fb51d8fc0
akinloluwami <akinkunmioye42@gmail.com>
2024-02-27 07:16:16 +0100

    add title and loading props

--- layouts/Dashboard.tsx
+++ layouts/Dashboard.tsx
@@ -1,8 +1,18 @@
+import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { FormEvent, ReactNode } from "react";
+import { CgSpinnerTwo } from "react-icons/cg";
 
-const DashboardLayout = ({ children }: { children: ReactNode }) => {
+const DashboardLayout = ({
+  children,
+  loading,
+  title,
+}: {
+  children: ReactNode;
+  loading?: boolean;
+  title: string;
+}) => {
   const pages = [
     { name: "Dashboard", route: "/dashboard" },
     { name: "Write", route: "/write" },
@@ -12,37 +22,50 @@ const DashboardLayout = ({ children }: { children: ReactNode }) => {
   const router = useRouter();
 
   return (
-    <div className="flex">
-      <div className="flex flex-col text-xl gap-y-7 px-20 py-5 border-r border-dashed border-that-grey w-1/4 h-screen fixed">
-        <h3 className="font-bold text-2xl">keydown</h3>
-        <div className="flex flex-col text-xl gap-y-12 mt-10">
-          {pages.map((page, i) => (
-            <Link href={page.route} key={i} className="w-fit">
-              {router.pathname.split("/")[1] ===
-                page.route.replace("/", "") && (
-                <span className="mr-2">&gt;</span>
-              )}
-              {page.name}
-            </Link>
-          ))}
-          <form
-            action="/api/logout"
-            method="POST"
-            onSubmit={async (e: FormEvent<HTMLFormElement>) => {
-              e.preventDefault();
-              const formElement = e.target as HTMLFormElement;
-              await fetch(formElement.action, {
-                method: formElement.method,
-              });
-              router.push("/login");
-            }}
-          >
-            <button className="text-left w-fit">Logout</button>
-          </form>
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <div className="flex">
+        <div className="flex flex-col text-xl gap-y-7 px-20 py-5 border-r border-dashed border-that-grey w-1/4 h-screen fixed">
+          <h3 className="font-bold text-2xl">keydown</h3>
+          <div className="flex flex-col text-xl gap-y-12 mt-10">
+            {pages.map((page, i) => (
+              <Link href={page.route} key={i} className="w-fit">
+                {router.pathname.split("/")[1] ===
+                  page.route.replace("/", "") && (
+                  <span className="mr-2">&gt;</span>
+                )}
+                {page.name}
+              </Link>
+            ))}
+            <form
+              action="/api/logout"
+              method="POST"
+              onSubmit={async (e: FormEvent<HTMLFormElement>) => {
+                e.preventDefault();
+                const formElement = e.target as HTMLFormElement;
+                await fetch(formElement.action, {
+                  method: formElement.method,
+                });
+                router.push("/login");
+              }}
+            >
+              <button className="text-left w-fit">Logout</button>
+            </form>
+          </div>
+        </div>
+        <div className="pt-5 pb-24 px-20 w-full ml-[25%]">
+          {loading ? (
+            <div className="flex justify-center items-center h-[calc(100vh-150px)]">
+              <CgSpinnerTwo className="animate-spin text-that-grey" size={50} />
+            </div>
+          ) : (
+            children
+          )}
         </div>
       </div>
-      <div className="pt-5 pb-24 px-20 w-full ml-[25%]">{children}</div>
-    </div>
+    </>
   );
 };
 
