c2e8a6512ddaad33878d3df8d095cd0d8351efc0
akinloluwami <akinkunmioye42@gmail.com>
2024-02-28 04:14:33 +0100

    .

--- pages/write.tsx
+++ pages/write.tsx
@@ -6,21 +6,20 @@ const Write = () => {
   return (
     <DashboardLayout title="Write">
       <div className="flex mt-5 flex-col gap-y-5">
-        <div className="flex justify-between items-center w-full">
-          <input
-            type="text"
-            className="bg-transparent border border-dashed border-that-grey text-3xl p-3 placeholder:text-that-grey font-semibold w-[50%]"
-            placeholder="Post title here..."
-          />
-          <div className="flex items-center gap-x-5">
-            <button className="text-that-grey-1 font-semibold text-xl">
-              Save Draft
-            </button>
-            <button className="bg-white text-black py-3 font-semibold text-xl flex items-center justify-center disabled:opacity-60 disabled:cursor-not-allowed transition-opacity px-10">
-              Publish
-            </button>
-          </div>
+        <div className="flex items-center gap-x-5 justify-end">
+          <button className="text-that-grey-1 font-semibold text-xl">
+            Save Draft
+          </button>
+          <button className="bg-white text-black py-3 font-semibold text-xl flex items-center justify-center disabled:opacity-60 disabled:cursor-not-allowed transition-opacity px-10">
+            Publish
+          </button>
         </div>
+        <input
+          type="text"
+          className="bg-transparent border border-dashed border-that-grey text-2xl p-3 placeholder:text-that-grey font-semibold w-full"
+          placeholder="Post title here..."
+        />
+
         <Editor />
       </div>
     </DashboardLayout>
