4c9bab72d899f8eb57dd3521a72dfb9fda8d8017
akinloluwami <akinkunmioye42@gmail.com>
2024-03-07 11:07:20 +0100

    ___

--- pages/api/posts/new.ts
+++ pages/api/posts/new.ts
@@ -1,6 +1,7 @@
 import { db, posts, users } from "@/lib/db";
 import { validateRequest } from "@/utils/validateRequest";
 import { eq } from "drizzle-orm";
+import { generateId } from "lucia";
 import { NextApiRequest, NextApiResponse } from "next";
 import { isURL } from "validator";
 
@@ -26,11 +27,27 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     return;
   }
 
+  if (!content) {
+    res.status(400).json({ message: "Post content is required" });
+  }
+
+  const isHtml = (content: string) => {
+    return content.startsWith("<") && content.endsWith("/>");
+  };
+
+  if (!isHtml(content)) {
+    res.status(400).json({ message: "Invalid post content" });
+  }
+
   try {
     await db.insert(posts).values({
+      id: generateId(32),
+      userId: user.id,
       title,
-      content,
       slug: generateSlug(title),
+      content,
+      coverImage,
+      isDraft,
     });
   } catch (error) {}
 };
