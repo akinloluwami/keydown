8ccc658f9212192051aef711f5f8be6966719c65
akinloluwami <akinkunmioye42@gmail.com>
2024-03-20 14:13:30 +0100

    ___

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -1,6 +1,28 @@
 import { NodeViewContent, NodeViewWrapper } from "@tiptap/react";
-export default () => (
+
+export const CodeBlock = ({
+  node: {
+    attrs: { language: defaultLanguage },
+  },
+  updateAttributes,
+  extension,
+}: any) => (
   <NodeViewWrapper className="relative">
+    {/* <select
+      contentEditable={false}
+      defaultValue={defaultLanguage}
+      onChange={(event) => updateAttributes({ language: event.target.value })}
+    >
+      <option value="null">auto</option>
+      <option disabled>—</option>
+      {extension.options.lowlight
+        .listLanguages()
+        .map((lang: string, index: number) => (
+          <option key={index} value={lang}>
+            {lang}
+          </option>
+        ))}
+    </select> */}
     <pre>
       <NodeViewContent as="code" />
     </pre>
